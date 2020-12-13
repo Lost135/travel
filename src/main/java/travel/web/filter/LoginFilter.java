b27630d6e0b50c795c9aa09d05804703379cb25a
lost <1420335820@qq.com>
2020-12-13 15:36:04 +0800

    travel 0.5.0

--- src/main/java/travel/web/filter/LoginFilter.java
+++ src/main/java/travel/web/filter/LoginFilter.java
@@ -2,10 +2,17 @@ package travel.web.filter;
 
 import travel.domain.User;
 
-import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
 
 @WebFilter(filterName = "Filter1",value = "/*")
 public class LoginFilter implements Filter {
@@ -14,14 +21,15 @@ public class LoginFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) req;
-
+        HttpServletResponse response = (HttpServletResponse) resp;
         String uri = request.getRequestURI();
+        String path = request.getContextPath();
         if(uri.contains("/favoriterank.html") || uri.contains("/myfavorite.html") ){
             User user = (User)request.getSession().getAttribute("user");
             if(user != null){
                 chain.doFilter(req, resp);
             }else {
-                request.getRequestDispatcher("/login.html").forward(request,resp);
+                response.sendRedirect(path+"/login.html");
             }
         }else {
             chain.doFilter(req, resp);
